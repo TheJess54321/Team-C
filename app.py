a4307003c06ded41cc45d06d2b5bb21620366cbe
TheJess54321 <aimanjasmin762@gmail.com>
2021-04-10 14:47:54 +0800

    test

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-
+port = int(os.environ.get('PORT', 33507))
 @app.route('/')
 def home():
     return render_template('home.html')
