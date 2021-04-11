a12831d7dae64fdbc651b30beb40fc1bc1122235
TheJess54321 <aimanjasmin762@gmail.com>
2021-04-11 10:29:29 +0800

    bismillah

--- app.py
+++ app.py
@@ -11,7 +11,23 @@ def home():
 
 @app.route('/about/')
 def about():
-    return render_template('about.html')
+    return render_template('home.html')
+
+    @app.route('/home1/')
+def home1():
+    return render_template('home1.html')
+
+    @app.route('/home2/')
+def home2():
+    return render_template('home2.html')
+
+    @app.route('/home3/')
+def home3():
+    return render_template('home3.html')
+
+    @app.route('/home4/')
+def home4():
+    return render_template('home4.html')
 
 
 if __name__ == '__main__':
