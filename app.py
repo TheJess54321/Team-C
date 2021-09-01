33ca74047119684923b0e942cf2b7778efb78b62
TheJess54321 <81469649+TheJess54321@users.noreply.github.com>
2021-09-01 08:19:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 port = int(os.environ.get('PORT', 33507))
 
-@app.route('/')
+@app.route('/home/')
 def home():
-    return render_template('about.html')
+    return render_template('home.html')
 
 @app.route('/about/')
 def about():
-    return render_template('home.html')
+    return render_template('about.html')
 
 @app.route('/home1/')
 def home1():
